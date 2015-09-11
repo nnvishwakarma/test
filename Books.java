5238e6ade4b314c37a7caff562ac1b4a1fa3bb1e
nnvishwakarma <nnvishwakarma@gmail.com>
2015-09-11 16:15:06 +0530

    Update Books.java

--- Books.java
+++ Books.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,21 +17,97 @@ public class Book {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long bookId;
 	private String title;
-	private List<String> authors;
+	@OneToMany	
+	private List<Author> authors;
 	private int edition;
 	private String description;
+	@OneToMany	
 	private List<Content> contents;
+	@ManyToOne
 	private Publisher publisher;
+	private String preface;
 	
 	public Book(){}
 	
-	public Book(Long bookId, String title, List<String> authors, int edition, String description,
-			List<Content> contents, Publisher publisher ){
+	public Book(Long bookId, String title, List<Author> authors, int edition, String description,
+			List<Content> contents, Publisher publisher,String preface){
 		this.bookId = bookId;
 		this.title = title;
 		this.authors = authors;
-		
+		this.edition = edition;
+		this.description = description;
+		this.contents = contents;
+		this.publisher = publisher;		
+		this.preface = preface;
 	}
 	
+	public String toString(){
+		return String.format("Book: [%d, %s, %s, %d, %s, %s ]",
+				getBookId(), getTitle(), getAuthors(), getEdition(), getDescription(), getPublisher().toString()
+				); 
+	}
+	public Long getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Long bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+
+	public int getEdition() {
+		return edition;
+	}
+
+	public void setEdition(int edition) {
+		this.edition = edition;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Content> getContents() {
+		return contents;
+	}
+
+	public void setContents(List<Content> contents) {
+		this.contents = contents;
+	}
+
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getPreface() {
+		return preface;
+	}
+
+	public void setPreface(String preface) {
+		this.preface = preface;
+	}	
 	
 }
